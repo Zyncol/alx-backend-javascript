c303de9801ced41df837f1b14920b80709ca1b6b
Zyncol <isaacyilipochipofya@gmail.com>
2025-01-29 23:23:57 +0200

    eslint fixing

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function asyncUploadUser(){
+export default async function asyncUploadUser() {
   let res = {};
 
   try {
@@ -10,5 +10,5 @@ export default async function asyncUploadUser(){
   } catch (err) {
     res = { photo: null, user:null };
   }
- return res;
+  return res;
 }
