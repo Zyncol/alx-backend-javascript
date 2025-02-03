aba3b7706ac10caa47e472c0f0dc118afef0271b
Zyncol <isaacyilipochipofya@gmail.com>
2025-02-04 00:25:23 +0200

    For the remaining basic node js files

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,11 @@
+/**
+ * this is for app controller
+ */
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello ALX!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
